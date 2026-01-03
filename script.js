022f76e863e1b99a3e296b8a303c1d8798645321
prasanth844 <135524731+prasanth844@users.noreply.github.com>
2026-01-03 12:44:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 //your code here
 
+function swapTheme() {
+    const app = document.getElementById("app");
+    const button = document.getElementById("swap");
+
+    if (app.classList.contains("day")) {
+        app.classList.remove("day");
+        app.classList.add("night");
+
+        button.classList.remove("button_day");
+        button.classList.add("button_night");
+    } else {
+        app.classList.remove("night");
+        app.classList.add("day");
+
+        button.classList.remove("button_night");
+        button.classList.add("button_day");
+    }
+}
